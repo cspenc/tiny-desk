1cec37ad95a5e6f3eec8f0f24d1a476671ac8e45
cspenc <crs992@gmail.com>
2017-03-20 19:20:49 -0400

    some initial changes

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+
+import VideoPlayer from './component/video_player';
+
 import './App.css';
 
 class App extends Component {
@@ -7,12 +9,11 @@ class App extends Component {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Tiny Desk Player</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div>
+          <VideoPlayer />
+        </div>
       </div>
     );
   }
---
+++ src/component/video_player.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class VideoPlayer extends Component {
+  render() {
+    return (
+      <div>
+        YO THIS IS WHERE THE PLAYER WILL BE HEHE
+      </div>
+    )
+  }
+}
+
+export default VideoPlayer;
