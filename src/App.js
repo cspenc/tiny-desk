dd6e4964eccb0b135f05b7c581ecb8b46cb90330
cspenc <crs992@gmail.com>
2017-05-24 19:44:15 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import YTSearch from 'youtube-api-search';
+import './App.css';
 
 import VideoPlayer from './component/video_player';
+import SearchBar from './component/search_bar';
 
-import './App.css';
+const API_KEY = 'AIzaSyCLVi6RhA8MtQZ0jHBU-ZwCEArURcEL6QY';
 
 class App extends Component {
   render() {
@@ -13,6 +15,7 @@ class App extends Component {
           <h2>Tiny Desk Player</h2>
         </div>
         <div>
+          <SearchBar />
           <VideoPlayer />
         </div>
       </div>
